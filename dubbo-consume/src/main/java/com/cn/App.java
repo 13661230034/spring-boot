2430d778877881e5f38173c0ddc7a8ed73f90004
1366123034@163.com <13661230034@163.com>
2018-06-01 14:46:19 +0800

    1.0.3
    更新boot框架添加配置文件，新增main方法

---
+++ dubbo-consume/src/main/java/com/cn/App.java
@@ -0,0 +1,18 @@
+package com.cn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+/**
+ * 启动类
+ * @author ynt
+ *
+ */
+@SpringBootApplication
+public class App {
+
+	public static void main(String[] args) {
+		SpringApplication.run(App.class, args);
+	}
+}
