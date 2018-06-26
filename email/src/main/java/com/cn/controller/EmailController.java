e6558f15cfe8caec99e3a2d74720b51f310dbc5e
1366123034@163.com <13661230034@163.com>
2018-06-26 17:00:35 +0800

    1.0.8

--- email/src/main/java/com/cn/controller/EmailController.java
+++ email/src/main/java/com/cn/controller/EmailController.java
@@ -3,8 +3,6 @@ package com.cn.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 import com.cn.service.impl.EmailServiceImp;
 @Component
